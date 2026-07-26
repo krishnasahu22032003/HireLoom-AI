7fa5006c23bd5fcda27db90bc05f234838523e13
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-26 23:41:34 +0530

    start sever function ad

--- backend/gateway/src/lib/startServer.ts
+++ backend/gateway/src/lib/startServer.ts
@@ -1,13 +1,17 @@
 import type { Express } from "express";
+import ENV_SECRETS from "./Secrets.js";
 
-function startServer(app: Express) {
+export function startServer(app: Express) {
+  const port = ENV_SECRETS.PORT;
 
-try{
+  const server = app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+  });
 
-}catch(error){
-    
-}
+  server.on("error", (error) => {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  });
 
-
-
-}
+  return server;
+};
