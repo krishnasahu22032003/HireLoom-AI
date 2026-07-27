82190f3edb09d2c6ea1840cf80ebf8e8981bd71e
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-27 23:29:54 +0530

    auth service startserver function add

---
+++ backend/services/auth/src/lib/startServer.ts
@@ -0,0 +1,17 @@
+import type { Express } from "express";
+import ENV_SECRETS from "./Secrets.js";
+
+export function startServer(app: Express) {
+  const port = ENV_SECRETS.PORT || 4001;
+
+  const server = app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+  });
+
+  server.on("error", (error) => {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  });
+
+  return server;
+};
