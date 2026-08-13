8882bbc3a1a4bf195a7c3031d89da2de8aa836a5
krishna sahu <krishna.sahu.work@gmail.com>
2026-08-13 19:23:31 +0530

    user signup controller create

--- backend/services/auth/src/controllers/user.controller.ts
+++ backend/services/auth/src/controllers/user.controller.ts
@@ -1,14 +1,77 @@
-import type { Request , Response } from "express";
+import type { Request, Response } from "express";
 import User from "../models/user.model.js";
+import { signupSchema } from "../validation/userValidation.js";
+import bcrypt from "bcrypt";
 
 // 1. signup 2. login 3. logout using jwt these i want 
 
-export function UserSignUp(req:Request , res:Response){
+const SALT_ROUNDS = 12;
 
+export async function UserSignUp(req: Request, res: Response) {
 
+    const userData = signupSchema.safeParse(req.body);
 
+    if (!userData.success) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid Credentials",
+            error: userData.error.flatten()
+        });
+    };
 
+    const { username, email, password, confirmPassword } = userData.data;
 
- }
+    try {
+
+        const checkUser = await User.find({
+            email
+        });
+
+        if (checkUser) {
+            return res.status(400).json({
+                success: false,
+                message: "Email Exits Please Choose Some Other Email"
+            })
+        };
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Password Does not match"
+            });
+        };
+
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User not created"
+            });
+        };
+
+        return res.status(201).json({
+            success: true,
+            message: "User Created Successfully",
+            data: {
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server Error"
+        });
+    };
+};
 
 
