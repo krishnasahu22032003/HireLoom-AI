ed87b209415e33826ccf0e88ea8951fed93db159
krishna sahu <krishna.sahu.work@gmail.com>
2026-08-29 03:09:36 +0530

    container component add

---
+++ frontend/src/components/ui/Container.tsx
@@ -0,0 +1,23 @@
+import type React from "react"
+import cn from "../../lib/utils"
+
+
+type ContainerProps = {
+
+    children : React.ReactNode , 
+    classname:string
+} ; 
+
+const Container = ({children , classname} : ContainerProps) => {
+    
+  return (
+    <div className={cn(
+        "mx-auto mx-w-2xl 2-full px-4 sm:px-6 lg:px-8",
+        classname
+    )}>
+        {children}
+    </div>
+  );
+};
+
+export default Container ;
